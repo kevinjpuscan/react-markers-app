f73589a8967b773994621c2982412255689e5b05
kevin puscan <kevinjpuscan@gmail.com>
2020-06-16 21:46:30 -0500

    Updates

---
+++ precache-manifest.26d7d4dcbf08f5c5986ab6c1cee9100c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac8906f8d4cfe7c882726e8d0b8c7ea",
+    "url": "/react-markers-app/index.html"
+  },
+  {
+    "revision": "f250457a95775b50feab",
+    "url": "/react-markers-app/static/css/main.c1e4a6eb.chunk.css"
+  },
+  {
+    "revision": "601a321dc8d384f93efa",
+    "url": "/react-markers-app/static/js/2.eb225e29.chunk.js"
+  },
+  {
+    "revision": "17d9da4602f87533bd4875e40d24d61a",
+    "url": "/react-markers-app/static/js/2.eb225e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f250457a95775b50feab",
+    "url": "/react-markers-app/static/js/main.e7fb63d4.chunk.js"
+  },
+  {
+    "revision": "a93d25253120e6373b19",
+    "url": "/react-markers-app/static/js/runtime-main.75a297ce.js"
+  }
+]);
